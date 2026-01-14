3c3d698306a06f3e523bde514d4e946f73003b3a
psso83 <pkse11283@g.hongik.ac.kr>
2026-01-14 17:04:34 +0900

    feat : ModelAttribute 방식 + POST 요청 받는 컨트롤러 작성법

--- src/main/java/com/meta/springmvc/entity/Star.java
+++ src/main/java/com/meta/springmvc/entity/Star.java
@@ -1,14 +1,14 @@
 package com.meta.springmvc.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-
+@Setter
 public class Star {
     String name;
     int age;
